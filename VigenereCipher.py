492435382e128e8552c67f05565b539f9bb73e3f
jjaenim <joshuatead@gmail.com>
2023-04-21 20:41:08 +0800

    Prints the output

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,6 +1,3 @@
-# Encrypt the message
-ciphertext = ""
-for i in range(len(message)):
-        encrypted_num = (ord(message[i]) - 65 + key_map[i % len(key_map)]) % 26
-        ciphertext += chr(encrypted_num + 65)
+# Output of the ciphertext
+print("The ciphertext is: " +ciphertext)
 print("")
