4da73412c6ab510621b2526aa78d1a8fc9c64bc3
jjaenim <joshuatead@gmail.com>
2023-04-21 20:37:08 +0800

    Creating the header

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,5 +1,17 @@
+# Daet, Joshua Adrian O.
+# Assignment No. 2
+# Question No. 3
+
+
+# This program will encrypt a message using the Vigenère Cipher
 # Create a header
-# Ask user to input the message and the key
-# Translate the key into corresponding letter values 0 - 25
-# Encrypt the message
-# Prints the output
+print("")
+name = "Hi! I am Joshua Adrian O. Daet of BSCpE 1-4"
+print(name.center(158))
+
+sentence = "This is a program where you can cipher text with Vigenère Cipher"
+print("")
+print(sentence.rjust(110))
+print("")
+print("-------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("")
