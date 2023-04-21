59d71251498c5091e636ea38820c6d27768b44a8
jjaenim <joshuatead@gmail.com>
2023-04-21 20:37:58 +0800

    Asks the user to input the message and thhe key

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,17 +1,10 @@
-# Daet, Joshua Adrian O.
-# Assignment No. 2
-# Question No. 3
-
-
-# This program will encrypt a message using the Vigenère Cipher
-# Create a header
+# Ask the user for the message and the key
+message = input("Please enter the message: ")
+message = message.upper().replace(" ", "")
 print("")
-name = "Hi! I am Joshua Adrian O. Daet of BSCpE 1-4"
-print(name.center(158))
-
-sentence = "This is a program where you can cipher text with Vigenère Cipher"
+print("Your message is: " +message)
 print("")
-print(sentence.rjust(110))
+key = input("Please enter the key: ")
+key = key.upper().replace(" ", "")
 print("")
-print("-------------------------------------------------------------------------------------------------------------------------------------------------------------")
-print("")
+print("Your key is: " +key)
